849068509727ae570fb769a184d35152bf07d360
Alif <abdullahalif0202@gmail.com>
2025-10-07 23:28:24 +0600

    Starting Server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func startHendler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server Works")
+}
 
 func main() {
-	http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/start", startHendler)
+	fmt.Println("Server is running on :8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println(" Unfortunately Error Occurs", err)
+	}
+
 }
