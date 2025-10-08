ba1402efaf220d046410de74012a0086978e22df
Alif <abdullahalif0202@gmail.com>
2025-10-08 23:47:12 +0600

    Creating struct

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type CheckReq struct {
+	JWT    string `json:"jwt"`
+	Secret string `json:"secret"`
+}
+
+type ReturnResponse struct {
+	Header         json.RawMessage `json:"header,omitempty"`
+	Payload        json.RawMessage `json:"payload,omitempty"`
+	ValidSignature bool            `json:"valid_signature"`
+	Error          string          `json:"error,omitempty"`
+}
 
 func startHendler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Server Works")
